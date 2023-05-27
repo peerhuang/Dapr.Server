1671d054a47f14818ed34939e7990b818803c95f
Huang Jie <peer_huang@msn.com>
2023-05-27 21:01:22 +0800

    ddd

--- Dapr.Grpc.Server/Grpc/HelloService.cs
+++ Dapr.Grpc.Server/Grpc/HelloService.cs
@@ -1,24 +1,22 @@
-﻿using Dapr.Client.Autogen.Grpc.v1;
+﻿using Dapr.AppCallback.Autogen.Grpc.v1;
+using Dapr.Client.Autogen.Grpc.v1;
 using Grpc.Core;
 
-namespace Dapr.Grpc.Server
+public class HelloService : AppCallback.AppCallbackBase
 {
-    public class HelloService : AppCallback.AppCallbackBase
+    public override async Task<InvokeResponse> OnInvoke(InvokeRequest request, ServerCallContext context)
     {
-        public override async Task<InvokeResponse> OnInvoke(InvokeRequest request, ServerCallContext context)
+        switch (request.Method)
         {
-            switch (request.Method)
-            {
-                case "sayhi":
-                    var response = new InvokeResponse();
-                    //var input = ProtobufHelper.Deserialize<HelloRequest>(request.Data.Value);
-                    //response.Data.Value = ProtobufHelper.Serialize(new HelloReply { Message = "ok" });
-                    return response;
+            case "sayhi":
+                var response = new InvokeResponse();
+                //var input = ProtobufHelper.Deserialize<HelloRequest>(request.Data.Value);
+                //response.Data.Value = ProtobufHelper.Serialize(new HelloReply { Message = "ok" });
+                return response;
 
-                default:
+            default:
 
-                    return await base.OnInvoke(request, context);
-            }
+                return await base.OnInvoke(request, context);
         }
     }
 }
--- Dapr.Grpc.Server/Program.cs
+++ Dapr.Grpc.Server/Program.cs
@@ -1,5 +1,3 @@
-using Dapr.Grpc.Server;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
