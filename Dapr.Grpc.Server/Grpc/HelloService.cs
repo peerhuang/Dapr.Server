1cb561098f342da492b563c0b36d02a6f199e165
Huang Jie <peer_huang@msn.com>
2023-05-27 20:53:46 +0800

    ddd

---
+++ Dapr.Grpc.Server/Grpc/HelloService.cs
@@ -0,0 +1,24 @@
+﻿using Dapr.Client.Autogen.Grpc.v1;
+using Grpc.Core;
+
+namespace Dapr.Grpc.Server
+{
+    public class HelloService : AppCallback.AppCallbackBase
+    {
+        public override async Task<InvokeResponse> OnInvoke(InvokeRequest request, ServerCallContext context)
+        {
+            switch (request.Method)
+            {
+                case "sayhi":
+                    var response = new InvokeResponse();
+                    //var input = ProtobufHelper.Deserialize<HelloRequest>(request.Data.Value);
+                    //response.Data.Value = ProtobufHelper.Serialize(new HelloReply { Message = "ok" });
+                    return response;
+
+                default:
+
+                    return await base.OnInvoke(request, context);
+            }
+        }
+    }
+}
