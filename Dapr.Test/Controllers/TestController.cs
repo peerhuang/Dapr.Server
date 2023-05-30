1a18d0f758cdba3a730f3db887dfd7f598ea9ce0
Huang Jie <peer_huang@msn.com>
2023-05-30 21:25:10 +0800

    ddd

---
+++ Dapr.Test/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dapr.Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+        private readonly DaprClient _daprClient;
+
+        public TestController(ILogger<TestController> logger, DaprClient daprClient)
+        {
+            _logger = logger;
+            _daprClient = daprClient;
+        }
+
+        public Task Get()
+        {
+        }
+    }
+}
