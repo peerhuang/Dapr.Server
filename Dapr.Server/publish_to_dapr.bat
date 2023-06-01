86a0e1c491d1fa904b25d4c9825ece2e875969d7
Huang Jie <peer_huang@msn.com>
2023-06-02 07:31:05 +0800

    ddd

--- Dapr.Server/publish_to_dapr.bat
+++ Dapr.Server/publish_to_dapr.bat
@@ -2,6 +2,6 @@ echo off
 echo "首先发布该Service项目，生成publish文件夹，然后到publish文件夹下双击执行该bat文件，即可发布服务到dapr"
 echo **********************************************************************************************
 cd /d %~dp0
-dapr stop Dapr-Test
+dapr stop Dapr-Server
 timeout /nobreak /t 3
-dapr run --app-id Dapr-Test --app-port 62020 --dapr-http-port 62021 -- dotnet Dapr.Test.dll --urls "http://*:62020"
+dapr run --app-id Dapr-Server --app-port 62010 --dapr-http-port 62011 -- dotnet Dapr.Test.dll --urls "http://*:62010"
