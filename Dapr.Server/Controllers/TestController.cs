daa85d8212e99898b7ab26cb0395755b45cf14bc
Huang Jie <peer_huang@msn.com>
2023-06-01 21:54:43 +0800

    dddd

--- Dapr.Server/Controllers/TestController.cs
+++ Dapr.Server/Controllers/TestController.cs
@@ -14,8 +14,22 @@ namespace Dapr.Server.Controllers
 
         [HttpGet]
         [Route("T1")]
-        public void Get()
+        public List<T1> GetT1(int? count)
         {
+            var output = new List<T1>();
+            for (var i = 0; i < (count ?? 10); i++)
+            {
+                output.Add(new T1()
+                {
+                    V1 = Random.Shared.Next(0, int.MaxValue).ToString(),
+                    V2 = Random.Shared.Next(0, int.MaxValue).ToString(),
+                    V3 = Random.Shared.Next(0, int.MaxValue).ToString(),
+                    V4 = Random.Shared.Next(0, int.MaxValue).ToString(),
+                    V5 = Random.Shared.Next(0, int.MaxValue).ToString(),
+                    V6 = Random.Shared.Next(0, int.MaxValue).ToString(),
+                });
+            }
+            return output;
         }
     }
 }
---
+++ Dapr.Server/T1.cs
@@ -0,0 +1,12 @@
+﻿namespace Dapr.Server
+{
+    public class T1
+    {
+        public string V1 { get; set; }
+        public string V2 { get; set; }
+        public string V3 { get; set; }
+        public string V4 { get; set; }
+        public string V5 { get; set; }
+        public string V6 { get; set; }
+    }
+}
