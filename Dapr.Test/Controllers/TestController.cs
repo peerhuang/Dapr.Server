0c4434151a3bcedbe48415cf62c657bd517e982e
Huang Jie <peer_huang@msn.com>
2023-06-03 18:13:46 +0800

    ddd

--- Dapr.Test/Controllers/TestController.cs
+++ Dapr.Test/Controllers/TestController.cs
@@ -20,19 +20,9 @@ namespace Dapr.Test.Controllers
         [Route("T1")]
         public async Task<int> GetT1()
         {
-            _logger.LogInformation("start");
             var request = _daprClient.CreateInvokeMethodRequest(HttpMethod.Get, "Dapr-Server", "/Test/T1");
             var data = await _daprClient.InvokeMethodAsync<List<T1>>(request);
-            _logger.LogInformation($"end({data.Count})");
             return data.Count;
-        }
-
-        [HttpGet]
-        [Route("T2")]
-        public async Task GetT2()
-        {
-            _logger.LogInformation("start");
-            await _daprClient.InvokeMethodGrpcAsync("Dapr-Server", "/Test/T1");
         }
     }
 }
